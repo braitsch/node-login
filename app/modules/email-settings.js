9575db71c6028609251da6593e5d592cabb2b258
Stephen Braitsch <stephen@quietless.com>
2012-06-01 16:25:52 -0700

    email-settings module

---
+++ app/modules/email-settings.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+	
+	host		: 'smtp.gmail.com',
+	user 		: 'your-email-address@gmail.com',
+	password 	: 'your-email-password',
+	sender		: 'Your Name <your-email-address@gmail.com>'
+	
+}
