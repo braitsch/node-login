db65c272d4b811866ed112ad85377e783378adc5
Stephen Braitsch <stephen@quietless.com>
2012-06-02 21:39:05 -0700

    adding analytics

--- app/core/config.js
+++ app/core/config.js
@@ -3,14 +3,14 @@ module.exports = function(app, exp) {
 
 	app.configure(function(){
 		app.set('views', app.root + '/app/views');
-		app.set('view engine', 'jade');	
-		app.set('view options', { pretty: true });		
+		app.set('view engine', 'jade');
+		app.set('view options', { pretty: true });
 		app.use(exp.bodyParser());
 		app.use(exp.cookieParser());
-		app.use(exp.session({ secret: 'super-duper-secret-secret' }));		
+		app.use(exp.session({ secret: 'super-duper-secret-secret' }));
 		app.use(exp.methodOverride());
-		app.use(require('stylus').middleware({ src: app.root + '/app' }));	
-		app.use(exp.static(app.root + '/app'));		
+		app.use(require('stylus').middleware({ src: app.root + '/app' }));
+		app.use(exp.static(app.root + '/app'));
 	});
 	
 }
