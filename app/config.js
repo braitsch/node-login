ef8f63e6efbaf9deaddf8a40bd068cb57ee21f18
Stephen Braitsch <stephen@quietless.com>
2012-06-14 12:56:33 -0700

    minor edit to config file

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ module.exports = function(app, exp) {
 	app.configure(function(){
 		app.set('views', app.root + '/app/server/views');
 		app.set('view engine', 'jade');
-		app.set('view options', { pretty: true });
+		app.set('view options', { doctype : 'html', pretty : true });
 		app.use(exp.bodyParser());
 		app.use(exp.cookieParser());
 		app.use(exp.session({ secret: 'super-duper-secret-secret' }));
