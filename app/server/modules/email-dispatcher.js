82ba97d14cdd04d825f71153511214dc79ef1507
braitsch <stephen@braitsch.io>
2016-04-22 10:29:22 -0700

    moving live demo over to heroku

--- app/server/modules/email-dispatcher.js
+++ app/server/modules/email-dispatcher.js
@@ -23,7 +23,7 @@ EM.dispatchResetPasswordLink = function(account, callback)
 
 EM.composeEmail = function(o)
 {
-	var link = 'http://node-login.braitsch.io/reset-password?e='+o.email+'&p='+o.pass;
+	var link = 'http://nodejs-login.herokuapp.com/reset-password?e='+o.email+'&p='+o.pass;
 	var html = "<html><body>";
 		html += "Hi "+o.name+",<br><br>";
 		html += "Your username is <b>"+o.user+"</b><br><br>";
